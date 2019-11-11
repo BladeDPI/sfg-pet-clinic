17f32786f668cd18ede0072df78ce413f2a85b1b
BladeDPI <istvan_doka@yahoo.com>
2019-11-11 13:50:42 +0100

    Create interfaces for Pet Clinic Services. Close #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
