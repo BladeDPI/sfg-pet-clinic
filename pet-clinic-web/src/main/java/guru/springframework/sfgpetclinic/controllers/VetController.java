de48998fd7c3ae8b225948ba2b6f1af57e0a35b6
BladeDPI <istvan_doka@yahoo.com>
2019-11-12 14:49:33 +0200

    Adding index page and controller for Vets and Owners. Close #12, #13

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({ "vets", "vets.html"})
+    public String index(){
+        return "vets/index";
+    }
+}
