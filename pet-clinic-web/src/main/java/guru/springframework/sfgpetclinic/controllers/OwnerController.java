73f6496f2b0ebeb788ff198d98fe4f24b852493a
BladeDPI <istvan_doka@yahoo.com>
2019-11-12 14:53:40 +0200

    Correction for Owners controller. Close #13

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,10 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({ "owners", "owners.html"})
+    @RequestMapping({ "", "/", "/index"})
     public String index(){
         return "owners/index";
     }
